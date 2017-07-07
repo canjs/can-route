60e4ebbee2eb6385bcc7cb0cf3d39e0279049104
Matthew Phillips <matthew@matthewphillips.info>
2017-07-07 08:09:33 -0400

    Make sure map decorator is only added once
    This prevents possible Max call stacks by ensuring that the map
    decorator is only added once. Close #92

--- test/route-test.js
+++ test/route-test.js
@@ -1120,6 +1120,25 @@ if (dev) {
 
 		dev.warn = oldlog;
 	});
+
+	test("setting route.data with the same map doesn't add the decorator function multiple times", function () {
+		var oldData = canRoute.data;
+		var map = new Map();
+		for(var i = 0; i < 10000; i++) {
+			canRoute.data = map;
+		}
+
+		canRoute.ready();
+
+		try {
+			map.attr("foo", "bar");
+			ok(true, "did not cause 'Maximum call stack size exceeded'");
+		} catch(err) {}
+		finally {
+			canRoute.data = oldData;
+		}
+	});
+
 }
 //!steal-remove-end
 
