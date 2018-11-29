a4c49e88c1338e5b557cb6117113ce5d40233962
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-11-29 12:53:24 -0700

    Refactored and commented stringify function.

--- src/string-coercion.js
+++ src/string-coercion.js
@@ -1,21 +1,36 @@
 var canReflect = require("can-reflect");
 var canSymbol = require("can-symbol");
 
-// Helper for convert any object (or value) to stringified object (or value)
+// # String Coercion Functions
+
+// ## stringify
+// Converts an object, array, Map or List to a string.
+// It attempts the following flow to convert to a string:
+// if `obj` is an object:
+//   - call `.serialize` on `obj`, if available
+//   - shallow copy `obj` using `.slice` or `can-reflect.assign`
+//   - convert each proprety to a string recursively
+// else
+//   - call `.toString` exist on `obj`, if available.
+
 function stringify(obj) {
-	// Object is array, plain object, Map or List
 	if (obj && typeof obj === "object") {
-		if (obj && typeof obj === "object" && ("serialize" in obj)) {
+		if ("serialize" in obj) {
 			obj = obj.serialize();
+
+		// Get array from array-like or shallow-copy object.
+		} else if (typeof obj.slice === "function") {
+			obj = obj.slice();
 		} else {
-			// Get array from array-like or shallow-copy object
-			obj = typeof obj.slice === "function" ? obj.slice() : canReflect.assign({}, obj);
+			canReflect.assign({}, obj);
 		}
-		// Convert each object property or array item into stringified new
-		canReflect.eachKey(obj, function (val, prop) {
+
+		// Convert each object property or array item into a string.
+		canReflect.eachKey(obj, function(val, prop) {
 			obj[prop] = stringify(val);
 		});
-		// Object supports toString function
+
+	// If `obj` supports `.toString` call it.
 	} else if (obj !== undefined && obj !== null && (typeof obj.toString === "function" )) {
 		obj = obj.toString();
 	}
@@ -23,6 +38,7 @@ function stringify(obj) {
 	return obj;
 }
 
+// ## stringCoercingMapDecorator
 // everything in the backing Map is a string
 // add type coercion during Map setter to coerce all values to strings so unexpected conflicts don't happen.
 // https://github.com/canjs/canjs/issues/2206
