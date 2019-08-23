47403a50e5e68c91802a3c5724813c28bbdc3cd2
Chasen Le Hara <git@chasenlehara.com>
2019-08-23 15:40:21 +0100

    Make can-observable-object the default route.data type
    - Update the docs to show `can-observable-object` as the default
    - Change `src/routedata.js` to use `can-observable-object` instead of `can-define/map/map`
    - Add tests specifically for `can-observable-object`
    - Update the `can-define/map/map` tests to not rely on the old default `data`
    
    We need to figure out IE compatibility. Right now `can-observable-object` is always imported because it’s the default type. I wonder if we should create a separate (define or legacy) module that IE users should import instead?
    
    Part of https://github.com/canjs/canjs/issues/5207

--- test/route-observable-iframe-test.js
+++ test/route-observable-iframe-test.js
@@ -1,9 +1,8 @@
 var canRoute = require("can-route");
-var DefineMap = require("can-define/map/map");
 var QUnit = require("steal-qunit");
 var canReflect = require("can-reflect");
 
-QUnit.module("can-route with can-define/map/map in an iframe", {
+QUnit.module("can-route with can-observable-object in an iframe", {
 	beforeEach: function() {
 		canRoute._teardown();
 		canRoute.urlData = canRoute.bindings.hashchange;
@@ -12,47 +11,44 @@ QUnit.module("can-route with can-define/map/map in an iframe", {
 	}
 });
 
-if (("onhashchange" in window)) {
-
-
-
+if ("onhashchange" in window) {
 	var teardownRouteTest;
-	var setupRouteTest = function(assert, callback){
-
+	var setupRouteTest = function(assert, callback) {
 		var testarea = document.getElementById("qunit-fixture");
 		var iframe = document.createElement("iframe");
 		var done = assert.async();
-		window.routeTestReady = function(){
+		window.routeTestReady = function() {
 			var args = canReflect.toArray(arguments);
 			args.unshift(iframe);
 			callback.apply(null, args);
 		};
-		iframe.src = __dirname + "/define-testing.html?"+Math.random();
+		iframe.src = __dirname + "/observable-testing.html?" + Math.random();
 		testarea.appendChild(iframe);
-		teardownRouteTest = function(){
-			setTimeout(function(){
+		teardownRouteTest = function() {
+			setTimeout(function() {
 				testarea.removeChild(iframe);
-				setTimeout(function(){
+				setTimeout(function() {
 					done();
-				},10);
-			},1);
+				}, 10);
+			}, 1);
 		};
 	};
 
-
 	if (typeof steal !== "undefined") {
 		QUnit.test("listening to hashchange (#216, #124)", function(assert) {
-
-			setupRouteTest(assert, function (iframe, iCanRoute) {
-
+			setupRouteTest(assert, function(iframe, iCanRoute) {
 				assert.ok(!iCanRoute.data.bla, "Value not set yet");
 
-				iCanRoute.bind("bla", function(){
-					assert.equal(iCanRoute.data.get("bla"), "blu", "Got route change event and value is as expected");
+				iCanRoute.bind("bla", function() {
+					assert.equal(
+						iCanRoute.data.get("bla"),
+						"blu",
+						"Got route change event and value is as expected"
+					);
 					teardownRouteTest();
 				});
 
-				iCanRoute._onStartComplete = function () {
+				iCanRoute._onStartComplete = function() {
 					iframe.src = iframe.src + "#!bla=blu";
 				};
 
@@ -61,54 +57,53 @@ if (("onhashchange" in window)) {
 		});
 
 		QUnit.test("updating the hash", function(assert) {
-			setupRouteTest(assert, function (iframe, iCanRoute, loc) {
-				iCanRoute._onStartComplete = function () {
+			setupRouteTest(assert, function(iframe, iCanRoute, loc) {
+				iCanRoute._onStartComplete = function() {
 					var after = loc.href.substr(loc.href.indexOf("#"));
-					assert.equal(after, "#!bar/" + encodeURIComponent("\/"));
+					assert.equal(after, "#!bar/" + encodeURIComponent("/"));
 
 					teardownRouteTest();
 				};
-
-				iCanRoute.data = new DefineMap();
 
 				iCanRoute.start();
 				iCanRoute.register("{type}/{id}");
 				iCanRoute.attr({
 					type: "bar",
-					id: "\/"
+					id: "/"
 				});
-
 			});
 		});
 
 		QUnit.test("unsticky routes", function(assert) {
-			setupRouteTest(assert, function (iframe, iCanRoute, loc) {
-
+			setupRouteTest(assert, function(iframe, iCanRoute, loc) {
 				iCanRoute.register("{type}");
 				iCanRoute.register("{type}/{id}");
 
-
-				iCanRoute._onStartComplete = function () {
+				iCanRoute._onStartComplete = function() {
 					iCanRoute.attr({
 						type: "bar"
 					});
 
-					setTimeout(function () {
+					setTimeout(function() {
 						var after = loc.href.substr(loc.href.indexOf("#"));
 						assert.equal(after, "#!bar");
 						iCanRoute.attr({
 							type: "bar",
-							id: "\/"
+							id: "/"
 						});
 
 						// check for 1 second
 						var time = new Date();
 						setTimeout(function innerTimer() {
 							var after = loc.href.substr(loc.href.indexOf("#"));
-							var isMatch = after === "#!bar/" + encodeURIComponent("\/");
+							var isMatch = after === "#!bar/" + encodeURIComponent("/");
 							var isWaitingTooLong = new Date() - time > 5000;
 							if (isMatch || isWaitingTooLong) {
-								assert.equal(after, "#!bar/" + encodeURIComponent("\/"), "should go to type/id "+ (new Date() - time));
+								assert.equal(
+									after,
+									"#!bar/" + encodeURIComponent("/"),
+									"should go to type/id " + (new Date() - time)
+								);
 								teardownRouteTest();
 							} else {
 								setTimeout(innerTimer, 30);
@@ -117,10 +112,7 @@ if (("onhashchange" in window)) {
 					}, 150);
 				};
 				iCanRoute.start();
-
 			});
 		});
-
 	}
-
 }
